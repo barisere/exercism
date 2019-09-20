252298b91053b23ed78c4f817de3cfc28a3e7d69
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-09-20 18:08:13 +0100

    Chain base conversions using Monad bind.

--- all-your-base/all_your_base.ml
+++ all-your-base/all_your_base.ml
@@ -20,11 +20,11 @@ let from_base_10 ~target ~digits =
     if List.exists ~f:(fun x -> x < 0) digits
     then None
     else let number = List.fold_left ~f:(fun acc x -> acc ^ Int.to_string x) ~init:"" digits |> Int.of_string in
-    let rec divide acc i =
-      let (quot, rem) = (i / n, i % n) in
-      if quot = 0 then rem::acc else divide (rem::acc)quot
-    in
-    Some (divide [] number)
+      let rec divide acc i =
+        let (quot, rem) = (i / n, i % n) in
+        if quot = 0 then rem::acc else divide (rem::acc)quot
+      in
+      Some (divide [] number)
   | _ -> None
 ;;
 
@@ -33,8 +33,5 @@ let convert_bases ~from ~digits ~target =
   match (from, target) with
   | (_, 10) -> to_base_10 ~from ~digits
   | (10, _) -> from_base_10 ~target ~digits
-  | _ -> let new_digits = to_base_10 ~from ~digits in
-    match new_digits with
-      Some x -> from_base_10 ~target ~digits:(x)
-    | None -> None
+  | _ -> Option.(to_base_10 ~from ~digits >>= (fun x -> from_base_10 ~target ~digits:x))
 ;;
