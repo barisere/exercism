0f8896fd1a1da6be5c5a35a2c034efcc4cb9ddc8
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-09-21 22:52:35 +0100

    Generalize solution as conversion from decimal using split_digits.
    Splitting digits using the same base maintains the original digits,
    thereby handling the cases where the target base is 10. Splitting
    digits by any other base converts the decimal number to that base.

--- all-your-base/all_your_base.ml
+++ all-your-base/all_your_base.ml
@@ -3,34 +3,19 @@ open Base;;
 type base = int
 ;;
 
+(** split_digits generalizes splitting a number into its component digits as
+    base conversion. The original digits are maintained if the target base is the same. *)
 let rec split_digits ?(base=10) ?(acc=[]) n =
   let (quot, rem) = (n / base, n % base) in
   if quot = 0 then rem::acc else split_digits ~base ~acc:(rem::acc) quot
 ;;
 
-let to_base_10 ~from ~digits =
-  let length = List.length digits in
-  let radix_to_pow pos n = n * from ** (length - pos - 1) in
-  List.foldi ~init:0 ~f:(fun pos acc x -> acc + radix_to_pow pos x) digits
-  |> split_digits
+let to_decimal ~from ~digits = List.fold_left ~f:(fun acc digit -> acc * from + digit) ~init:0 digits
 ;;
-
-let from_base_10 ~target ~digits =
-  List.fold_left ~f:(fun acc x -> acc ^ Int.to_string x) ~init:"" digits
-  |> Int.of_string
-  |> split_digits ~base:target
-;;
-
 
 let convert_bases ~from ~digits ~target =
   if List.exists ~f:(fun x -> x < 0 || x >= from) digits then None
   else if from < 2 || target < 2 then None
-  else let conv =
-         match (from, target) with
-         | (_, 10) -> to_base_10 ~from ~digits
-         | (10, _) -> from_base_10 ~target ~digits
-         | _ -> to_base_10 ~from ~digits |> (fun x -> from_base_10 ~target ~digits:x)
-    in
-    Some conv
+  else to_decimal ~from ~digits |> split_digits ~base:target |> Option.return
 ;;
 
