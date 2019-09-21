bb57cdab24b3cb92e88b50edb1100b1fab746be9
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-09-21 22:26:17 +0100

    Move validation into convert_bases.
    Now the Option type is returned from only convert_bases. No need
    to handle Option types from other functions.

--- all-your-base/all_your_base.ml
+++ all-your-base/all_your_base.ml
@@ -9,27 +9,28 @@ let rec split_digits ?(base=10) ?(acc=[]) n =
 ;;
 
 let to_base_10 ~from ~digits =
-  if List.exists ~f:(fun x -> x >= from || x < 0) digits then None
-  else let length = List.length digits in
-    let radix_to_pow pos n = n * from ** (length - pos - 1) in
-    List.foldi ~init:0 ~f:(fun pos acc x -> acc + radix_to_pow pos x) digits
-    |> split_digits
-    |> Option.return
+  let length = List.length digits in
+  let radix_to_pow pos n = n * from ** (length - pos - 1) in
+  List.foldi ~init:0 ~f:(fun pos acc x -> acc + radix_to_pow pos x) digits
+  |> split_digits
 ;;
 
 let from_base_10 ~target ~digits =
-  if List.exists ~f:(fun x -> x < 0) digits then None
-  else List.fold_left ~f:(fun acc x -> acc ^ Int.to_string x) ~init:"" digits
-       |> Int.of_string
-       |> split_digits ~base:target
-       |> Option.return
+  List.fold_left ~f:(fun acc x -> acc ^ Int.to_string x) ~init:"" digits
+  |> Int.of_string
+  |> split_digits ~base:target
 ;;
 
 
 let convert_bases ~from ~digits ~target =
-  match (from, target) with
-  | (f, t) when f < 2 || t < 2 -> None
-  | (_, 10) -> to_base_10 ~from ~digits
-  | (10, _) -> from_base_10 ~target ~digits
-  | _ -> Option.(to_base_10 ~from ~digits >>= (fun x -> from_base_10 ~target ~digits:x))
+  if List.exists ~f:(fun x -> x < 0 || x >= from) digits then None
+  else if from < 2 || target < 2 then None
+  else let conv =
+         match (from, target) with
+         | (_, 10) -> to_base_10 ~from ~digits
+         | (10, _) -> from_base_10 ~target ~digits
+         | _ -> to_base_10 ~from ~digits |> (fun x -> from_base_10 ~target ~digits:x)
+    in
+    Some conv
 ;;
+
