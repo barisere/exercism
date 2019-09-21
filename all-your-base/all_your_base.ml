a116f2383d4283de8f2bd22b8553799f24679b1f
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-09-21 22:05:06 +0100

    Generalize splitting digits as arbitrary base conversion.
    Use the general method of base conversion to split digits by
    converting to the same base.

--- all-your-base/all_your_base.ml
+++ all-your-base/all_your_base.ml
@@ -3,34 +3,32 @@ open Base;;
 type base = int
 ;;
 
+let rec split_digits ?(base=10) ?(acc=[]) n =
+  let (quot, rem) = (n / base, n % base) in
+  if quot = 0 then rem::acc else split_digits ~base ~acc:(rem::acc) quot
+;;
+
 let to_base_10 ~from ~digits =
-  match from with
-  | n when n > 1 ->
-    if List.exists ~f:(fun x -> x >= from || x < 0) digits
-    then None
-    else let length = List.length digits in
-      let radix_to_pow pos i = i * n ** (length - pos - 1) in
-      Some (List.foldi ~init:0 ~f:(fun pos acc x -> (radix_to_pow pos x)+acc) digits |> Int.to_string |> String.to_list |> List.map ~f:Char.get_digit_exn)
-  | _ -> None
+  if List.exists ~f:(fun x -> x >= from || x < 0) digits then None
+  else let length = List.length digits in
+    let radix_to_pow pos n = n * from ** (length - pos - 1) in
+    List.foldi ~init:0 ~f:(fun pos acc x -> acc + radix_to_pow pos x) digits
+    |> split_digits
+    |> Option.return
 ;;
 
 let from_base_10 ~target ~digits =
-  match target with
-  | n when n > 1 ->
-    if List.exists ~f:(fun x -> x < 0) digits
-    then None
-    else let number = List.fold_left ~f:(fun acc x -> acc ^ Int.to_string x) ~init:"" digits |> Int.of_string in
-      let rec divide acc i =
-        let (quot, rem) = (i / n, i % n) in
-        if quot = 0 then rem::acc else divide (rem::acc)quot
-      in
-      Some (divide [] number)
-  | _ -> None
+  if List.exists ~f:(fun x -> x < 0) digits then None
+  else List.fold_left ~f:(fun acc x -> acc ^ Int.to_string x) ~init:"" digits
+       |> Int.of_string
+       |> split_digits ~base:target
+       |> Option.return
 ;;
 
 
 let convert_bases ~from ~digits ~target =
   match (from, target) with
+  | (f, t) when f < 2 || t < 2 -> None
   | (_, 10) -> to_base_10 ~from ~digits
   | (10, _) -> from_base_10 ~target ~digits
   | _ -> Option.(to_base_10 ~from ~digits >>= (fun x -> from_base_10 ~target ~digits:x))
