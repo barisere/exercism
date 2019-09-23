b543deb266a55f293daf4a7b12c597a889e6b981
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-09-23 07:11:44 +0100

    Solution to Luhn exercise.

---
+++ luhn/luhn.ml
@@ -0,0 +1,23 @@
+open Base
+;;
+
+let double_second idx v =
+  if idx % 2 = 0 then
+    let d = v + v in
+    if d > 9 then d - 9 else d
+  else v
+
+let valid id_number =
+  if String.exists
+      ~f:(fun c -> not(Char.is_digit c || Char.is_whitespace c)) id_number
+  then false
+  else let id_number = String.filter ~f:Char.is_digit id_number in
+    if String.length id_number < 2 then false
+    else id_number
+         |> String.filter ~f:Char.is_digit
+         |> String.to_list_rev
+         |> List.mapi ~f:(fun idx v -> Char.get_digit_exn v |> double_second (idx + 1))
+         |> List.fold ~init:0 ~f:(+)
+         |> fun v -> v % 10 = 0
+;;
+
